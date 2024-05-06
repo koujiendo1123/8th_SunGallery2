81ef573fef4df271228690ed3118777154c7a4c8
dodolia1123 <dodolia1123@gmail.com>
2024-05-06 13:40:10 +0900

    ユーザーの新規作成ができなくなった。ログアウトはできるようになった。

--- app/controllers/general_users/sessions_controller.rb
+++ app/controllers/general_users/sessions_controller.rb
@@ -5,26 +5,25 @@ class GeneralUsers::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
    def new
-    @generaluser = GeneralUser.new
+     @generaluser = GeneralUser.new
      super
    end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-   def destroy
-    logout
-    redirect_to root_path, status: :see_other, success: 'ログアウトしました'
+   def create
      super
    end
 
-  # protected
+  # DELETE /resource/sign_out
+   def destroy
+    sign_out current_general_user
+    redirect_to root_path, status: :see_other, notice: 'ログアウトしました'
+   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  #  protected
+
+  #  # If you have extra params to permit, append them to the sanitizer.
+  #  def configure_sign_in_params
+  #    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  #  end
 end
---
+++ app/javascript/application.js
@@ -0,0 +1,3 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// WEBバックにも追記しているが念の為追記
+import "@hotwired/turbo-rails"
