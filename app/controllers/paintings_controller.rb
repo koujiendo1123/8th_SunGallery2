79bb9fc21bfac070a7c31514fbceba6f58234252
dodolia1123 <dodolia1123@gmail.com>
2024-05-16 21:26:01 +0900

    絵画アップロード機能を実装完了

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,13 +1,11 @@
 class PaintingsController < ApplicationController
+  before_action :set_painting, only: [:show, :edit, :update, :destroy, :print_images]
 
   def index
     @paintings = Painting.all
   end
 
-  def show
-    @painting = Painting.find(params[:id])
-  end
-  
+  def show; end
 
   def new
     @painting = Painting.new
@@ -15,19 +13,42 @@ class PaintingsController < ApplicationController
 
   def create
     @painting = Painting.new(permit_params)
+    Rails.logger.debug @painting.images.attached? # デバッグ情報を出力
     if @painting.save
-      flash[:success] = "Object successfully created"
-    redirect_to @painting 
+      flash[:success] = "絵画を登録しました"
+      redirect_to @painting 
     else
-      flash[:error] = "Something went wrong"
+      flash[:error] = "絵画を登録できませんでした"
       render 'new'
     end
   end
 
+  def update
+    if @painting.update(permit_params)
+      redirect_to painting_path, notice: "更新しました"
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @painting.destroy
+    redirect_to paintings_path, notice: "削除しました", status: :see_other
+  end
+
+  def print_images
+    @painting = Painting.find(params[:id])
+  end
+
   private
   
-  def permit_params
-    params.require(:painting).permit(:name, :author_id)
+  def set_painting
+    @painting = Painting.find(params[:id])
   end
-    
+
+  def permit_params
+    params.require(:painting).permit(:name, :author_id, images: []).tap do |permitted_params|
+      permitted_params[:images] = permitted_params[:images].reject(&:blank?)
+    end
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 // WEBバックにも追記しているが念の為追記
 // import "@hotwired/turbo-rails"
+function printImages() {
+  var content = document.getElementById('printable-images').innerHTML;
+  var originalContent = document.body.innerHTML;
+
+  document.body.innerHTML = content;
+  window.print();
+  document.body.innerHTML = originalContent;
+}
